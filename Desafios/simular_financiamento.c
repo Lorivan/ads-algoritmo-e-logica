478f4a8efc90b4e1e6027c8030cd1fc3563e543d
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-05-29 13:40:22 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

---
+++ Desafios/simular_financiamento.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int idade;
+    float valorRenda;
+    char restricao;
+
+    printf("Digite a idade: ");
+    scanf("%d",&idade);
+    printf("Digite o valor da renda mensal: ");
+    scanf("%f",&valorRenda);
+    printf("Possui restrição no nome? ");
+    scanf(" %c",&restricao);
+
+    if (idade >= 18 && valorRenda >= 3600.0 && restricao == 's' || restricao == 'S') {
+        printf("Financiamento Aprovado\n");
+    } else if (idade > 18) {
+        printf("Financiamento reprovado: idade menor que 18 anos\n");
+    } else if (valorRenda < 3600.00) {
+        printf("Financiamento recusado: renda insuficiente! \n");
+    } else {
+        printf("Financiamento reprovado: cliente com restricao! ");
+    }
+    return 0;
+}
