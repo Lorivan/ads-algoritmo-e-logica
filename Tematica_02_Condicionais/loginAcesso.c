176de46e874b0d19d3f0804cb04a001b81ca3e8a
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-05-29 12:35:06 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

--- Tematica_02_Condicionais/loginAcesso.c
+++ Tematica_02_Condicionais/loginAcesso.c
@@ -4,7 +4,7 @@ int main() {
     printf("Digite sua idade:\n");
     scanf("%d", &idade);
     if (idade >= 17) {
-        printf("Acesso negado liberado voce e mario de idade");
+        printf("Acesso negado liberado voce e menor de idade");
     }
     else {
         printf("Acesso negado voce e menor de idade");
