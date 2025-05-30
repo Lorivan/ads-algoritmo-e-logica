bba81add0aebd6d5318d0947d16325f8b3b0caba
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-05-29 21:12:36 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

--- Desafios/sistema_loja_online.c
+++ Desafios/sistema_loja_online.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
 float valorCompra;
-float descontoA;
-float descontoP;
-float valorFinal;
+float descontoA = 0.0;
+float descontoP = 0.0;
+float valorFinal1;
+float valorFinal2;
 int  tipoPagamento;
 
 printf("Digite o valor da compra\n: ");
@@ -17,7 +18,13 @@ if(valorCompra > 500.0 && tipoPagamento == 1 ){
  if(valorCompra <= 500 && tipoPagamento == 2){
   descontoP = valorCompra * 0.05; // 5% por cento
 }
-printf("O valor do desconto a vista R$: %.2f", descontoA);
+valorFinal1 = descontoA - valorCompra;
+valorFinal2 = descontoP - valorCompra;
+
+printf("\n Resumo da Compra\n");
+printf("O valor original R$: %.2f", valorCompra);
+printf(" valor do desconto a vista R$ %.2f", descontoA);
 printf("O valor do desconto parcelado R$: %.2f", descontoP);
+printf(" valor do final R$ %.2f", valorFinal1);
 return 0;
 }
