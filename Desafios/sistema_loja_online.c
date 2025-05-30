2f286df46aae99722183958a7198db6c13dd1a53
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-05-29 21:45:18 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

--- Desafios/sistema_loja_online.c
+++ Desafios/sistema_loja_online.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
 int main(){
 float valorCompra;
-float descontoA = 0.0;
-float descontoP = 0.0;
-float valorFinal1;
-float valorFinal2;
+float desconto = 0.0;
+float valorFinal;
 int  tipoPagamento;
 
-printf("Digite o valor da compra\n: ");
+printf("Digite o valor da compra: ");
 scanf("%f", &valorCompra);
-printf("Digite o tipo de pagamento (1 - À vista | 2 - Parcelado)\n:  " );
+printf("Digite o tipo de pagamento (1 - A vista | 2 - Parcelado):  " );
 scanf("%d", &tipoPagamento);
 
 if(valorCompra > 500.0 && tipoPagamento == 1 ){
- descontoA = valorCompra * 0.10; // 10% por cento
- }
- if(valorCompra <= 500 && tipoPagamento == 2){
-  descontoP = valorCompra * 0.05; // 5% por cento
+ desconto= valorCompra * 0.10; // 10% por cento
+ }  else if(valorCompra > 500 && tipoPagamento == 2){
+  desconto = valorCompra * 0.05; // 5% por cento
+} else if(valorCompra <= 500.0 && tipoPagamento == 1 ){
+ desconto = valorCompra * 0.05; // 10% por cento
+ } else if (valorCompra <= 500 && tipoPagamento == 2){
+  desconto = valorCompra * 0.02; // 5% por cento
 }
-valorFinal1 = descontoA - valorCompra;
-valorFinal2 = descontoP - valorCompra;
+valorFinal = valorCompra - desconto ;
 
-printf("\n Resumo da Compra\n");
-printf("O valor original R$: %.2f", valorCompra);
-printf(" valor do desconto a vista R$ %.2f", descontoA);
-printf("O valor do desconto parcelado R$: %.2f", descontoP);
-printf(" valor do final R$ %.2f", valorFinal1);
+printf("Resumo da Compra\n");
+printf("O valor original R$: %.2f\n", valorCompra);
+printf(" valor do desconto a vista R$ %.2f\n", desconto);
+printf("O valor do desconto parcelado R$: %.2f\n", desconto);
+printf(" valor final R$ %.2f", valorFinal);
+
 return 0;
 }
