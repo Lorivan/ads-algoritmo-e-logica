02509a0cdb95bc02f4511504685634294750d50b
Lorivan Lino Abreu <lorivan.lino@gmail.com>
2025-05-31 17:53:04 -0300

    Lorivan Lino de Abreu lorivan.lino@gmail.com

--- Desafios/sistema_loja_online.c
+++ Desafios/sistema_loja_online.c
@@ -1,31 +1,31 @@
-#include<stdio.h>
-int main(){
-float valorCompra;
-float desconto = 0.0;
-float valorFinal;
-int  tipoPagamento;
+    #include<stdio.h>
+    int main(){
+    float valorCompra;
+    float desconto = 0.0;
+    float valorFinal;
+    int  tipoPagamento;
 
-printf("Digite o valor da compra: ");
-scanf("%f", &valorCompra);
-printf("Digite o tipo de pagamento (1 - A vista | 2 - Parcelado):  " );
-scanf("%d", &tipoPagamento);
+    printf("Digite o valor da compra: ");
+    scanf("%f", &valorCompra);
+    printf("Digite o tipo de pagamento (1 - A vista | 2 - Parcelado):  " );
+    scanf("%d", &tipoPagamento);
 
-if(valorCompra > 500.0 && tipoPagamento == 1){
- desconto= valorCompra * 0.10; // 10% por cento
- }  else if(valorCompra > 500 && tipoPagamento == 2){
-  desconto = valorCompra * 0.05; // 5% por cento
-} else if(valorCompra <= 500.0 && tipoPagamento == 1){
- desconto = valorCompra * 0.05; // 5% por cento
- } else if (valorCompra <= 500 && tipoPagamento == 2){
-  desconto = valorCompra * 0.02; // 2% por cento
-}
-valorFinal = valorCompra - desconto ;
+    if(valorCompra >=500.0 && tipoPagamento == 1){
+     desconto= valorCompra * 0.10; // 10% por cento
+     }  else if(valorCompra >= 500 && tipoPagamento == 2){
+      desconto = valorCompra * 0.05; // 5% por cento
+    } else if(valorCompra <= 500.0 && tipoPagamento == 1){
+     desconto = valorCompra * 0.05; // 5% por cento
+     } else if (valorCompra <= 500 && tipoPagamento == 2){
+      desconto = valorCompra * 0.02; // 2% por cento
+    }
+    valorFinal = valorCompra - desconto ;
 
-printf("Resumo da Compra\n");
-printf("O valor original R$: %.2f\n", valorCompra);
-printf(" valor do desconto a vista R$ %.2f\n", desconto);
-printf("O valor do desconto parcelado R$: %.2f\n", desconto);
-printf(" valor final R$ %.2f", valorFinal);
+    printf("Resumo da Compra\n");
+    printf("O valor original R$: %.2f\n", valorCompra);
+    printf(" valor do desconto a vista R$ %.2f\n", desconto);
+    printf("O valor do desconto parcelado R$: %.2f\n", desconto);
+    printf(" valor final R$ %.2f", valorFinal);
 
-return 0;
-}
+    return 0;
+    }
